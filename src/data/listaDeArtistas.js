2b68c84b62d63be55fc9e967652b68ede908bee3
moreira-edu <ml.ri7u@gmail.com>
2022-06-03 03:15:01 -0300

    FIX: exclusão da img

--- src/data/listaDeArtistas.js
+++ src/data/listaDeArtistas.js
@@ -1,5 +1,10 @@
 export const lista = [
   {
+    nome: "Dupla TherezeAna",
+    data: "Domingo, das 17h às 21h",
+    image: "/assets/images/artistas/therezeana.PNG",
+  },
+  {
     nome: "Diguin Tenebroso",
     data: "Terça e Quinta das 19h às 22h",
     image: "/assets/images/artistas/rodrigo.png",
@@ -8,10 +13,5 @@ export const lista = [
     nome: "MC Javinha e DJ Capivara",
     data: "Terça e Quarta das 19h às 22h",
     image: "/assets/images/artistas/dupla.PNG",
-  },
-  {
-    nome: "Dupla TherezeAna",
-    data: "Domingo, das 17h às 21h",
-    image: "/assets/images/artistas/therezeana.PNG",
   },
 ];
