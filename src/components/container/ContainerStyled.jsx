ec5bb7b6302b46680baa211de9c43deb1932369e
Guilherme <carvalho_guilhermegomes@hotmail.com>
2022-06-06 19:42:22 -0300

    adicionando curiosidades

--- src/components/container/ContainerStyled.jsx
+++ src/components/container/ContainerStyled.jsx
@@ -1,11 +1,45 @@
 import styled from "styled-components";
 
 export const ContainerSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content:space-between;
   box-sizing: border-box;
-
   width: 100%;
+  padding: 1rem 4rem;
+`;
 
-  padding: 1rem 2rem;
+export const H = styled.h1`
+font-size: 2.2rem;
+color: #EA7C69;
+align-self: center;
+`;
+
+export const Ol = styled.ol`
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`;
+
+export const Li = styled.li`
+align-content: center;
+font-size: 1.6rem;
+padding: 1rem;
+`;
+
+export const P = styled.p`
+font-size:1.8rem;
+align-self: center;
+`;
+
+export const A = styled.a`
+color: #EA7C69;
+transition: width 3s;
+`;
+
+export const Span = styled.span`
+font-size: 1.9rem;
+color: #EA7C69;
 `;
 
 export const Borda = styled.div`
--- src/pages/Home/SectionHome.jsx
+++ src/pages/Home/SectionHome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import BoxApresentacao from "../../components/boxApresentação/BoxApresentacao";
-import { ContainerSection } from "../../components/container/ContainerStyled";
+import { ContainerSection, H, Li, P, Ol, A, Span } from "../../components/container/ContainerStyled";
 import { Helmet } from "react-helmet";
+import Sobre from "../Sobre/Sobre.jsx";
 const Home = () => {
   return (
     <>
@@ -13,6 +14,44 @@ const Home = () => {
         texto={"Drinks e Lanches únicos!"}
       />
       <ContainerSection>
+        <H>5 Curiosodades sobre PUBs:</H>
+        <Ol type="I">
+          <Li>
+            <Span>I-</Span> Os primeiros protótipos de pubs surgiram cerca de 2 mil anos
+            atrás, a partir da ida dos romanos para o Reino Unido. Foram eles que
+            construíram ao longo das estradas e cidades da Grã-Bretanha as
+            chamadas tavernas, onde eram comercializados vinhos.
+          </Li>
+          <Li>
+            <Span>II -</Span> De acordo com o Guinness Book of Records, o pub mais antigo da
+            Grã-Bretanha ainda em funcionamento é o Ye Olde Fighting Cocks. Ele
+            foi construído no século 11 e fica em St Albans, cidade situada ao
+            sul do condado de Hertfordshire. Recebeu este nome devido às brigas
+            de galo que eram comuns no local.
+          </Li>
+          <Li>
+            <Span>III -</Span> Foi no famoso Cavern Club, pub situado em Liverpool, que os
+            Beatles começaram a sua carreira.
+          </Li>
+          <Li>
+            <Span>IV -</Span> Aqui no Brasil, os pubs buscam reproduzir estilo, utilizando em
+            sua decoração madeira e bancos altos, deixando um estilo “taverna”,
+            inspirado no período medieval, e com pouca iluminação. Além disso, é
+            muito comum a venda de grande variedade de chope, inclusive de
+            várias marcas importantes, servido em canecas de um pint
+            (aproximadamente 570ml).
+          </Li>
+          <Li>
+            <Span>V -</Span> Para se adequar ao gosto do brasileiro, o pub passou por mudanças,
+            assim, foi a música ao vivo, comidas típicas brasileiras e jogos,
+            passaram a fazer parte dos pubs. Alguns até são temáticos,
+            inspirados nos mais diversos temas
+          </Li>
+          <P>
+            Quer saber mais sobre PUB Do Francês?
+            <A href={{ Sobre }}> Clique Aqui</A>
+          </P>
+        </Ol>
       </ContainerSection>
     </>
   );
